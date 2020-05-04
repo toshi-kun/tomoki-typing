9025b531ee95b9d92e66d72dd9a19f692ead2f33
toshi-kun <60562737+toshi-kun@users.noreply.github.com>
2020-05-04 11:55:24 +0900

    add click event into "Settei".

--- docs/js/settei.js
+++ docs/js/settei.js
@@ -2,9 +2,19 @@
 
   function init(){
     console.log("loaded");
+
+    let nodes = window.document.getElementsByClassName("difficulty");
+    for(var i=0 ; i<nodes.length ; i++ ){
+	//console.log(nodes[i].innerText);
+	nodes[i].addEventListener("click", click);
+    }
+
   };
 
-
+  function click(){
+      alert( this.innerText );
+      history.back();
+  };
 
 
   window.addEventListener('DOMContentLoaded', init );
